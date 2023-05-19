3f0ff424fdc2c38d696fc24a06470b20d19b236c
Jason Xu <Xinwen31415@gmail.com>
2023-05-18 22:27:57 -0500

    added logging to guard

--- src/utils/guards/auth.guard.ts
+++ src/utils/guards/auth.guard.ts
@@ -1,11 +1,15 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ConsoleLogger, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+	private _logger: ConsoleLogger = new ConsoleLogger('Auth');
+
 	public canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
 		const req = context.switchToHttp().getRequest<Request>();
+
+		this._logger.log(`Received auth attempt with cookie ${req.cookies['civdb:secret']}`);
 
 		return req.cookies['civdb:secret'] === process.env.SECRET;
 	}
